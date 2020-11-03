d9e9931092d393a42ca27491b0c3859f96cf90e9
708u <naoya.uda@showcase-gig.com>
2020-11-03 11:06:54 +0900

    feat: complete ajax app

---
+++ js_primer/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const obj = {
+    name: 'foo',
+    hello() {
+        return 'hello!';
+    }
+}
+
+const arr = [
+    'foo', 'bar', 'buz'
+]
+
+const heyhey = () => 'heyhey';
+
+console.log(`${obj.hello()}, baby`);
+// console.log(arr.some(obj => obj.name === 'barr'));
+
+const input = 'bar';
+test`foo ${0} ${1}`;
+function test(strings) {
+    strings.forEach((s) => console.log(s))
+}
