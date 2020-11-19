ea2d174eda51502f921f9803c5accf5d3191db95
708u <naoya.uda@showcase-gig.com>
2020-11-20 08:09:03 +0900

    feat: add square type

--- react-sample/react-tutorial-ts/src/components/square.tsx
+++ react-sample/react-tutorial-ts/src/components/square.tsx
@@ -1,8 +1,10 @@
 import React, { FC, MouseEvent } from 'react';
 
+export type square = 'O' | 'X' | null;
+
 type Props = {
   onClick: (e: MouseEvent) => void,
-  value: string
+  value: square
 }
 
 const Square: FC<Props> = (props) => {
