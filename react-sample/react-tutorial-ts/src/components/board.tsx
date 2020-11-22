9a0484dcd52b9d5713b1b70f76f5c24c5be81233
708u <naoya.uda@showcase-gig.com>
2020-11-22 18:34:59 +0900

    feat: install styled-componets

--- react-sample/react-tutorial-ts/src/components/board.tsx
+++ react-sample/react-tutorial-ts/src/components/board.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Square, { square } from 'components/square';
+import styled from 'styled-components';
 import 'index.css';
 
 export type board = Array<square>
@@ -9,25 +10,33 @@ type props = {
   onClick: (n: number) => void
 }
 
+const BoardRaw = styled.div`
+  ::after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+`
+
 const Board = (props: props) => {
   const { squares, onClick } = props;
   return (
     <div>
-      <div className="board-row">
+      <BoardRaw>
         <Square value={squares[0]} onClick={() => onClick(0)}/>
         <Square value={squares[1]} onClick={() => onClick(1)}/>
         <Square value={squares[2]} onClick={() => onClick(2)}/>
-      </div>
-      <div className="board-row">
+      </ BoardRaw>
+      <BoardRaw>
         <Square value={squares[3]} onClick={() => onClick(3)}/>
         <Square value={squares[4]} onClick={() => onClick(4)}/>
         <Square value={squares[5]} onClick={() => onClick(5)}/>
-      </div>
-      <div className="board-row">
+      </ BoardRaw>
+      <BoardRaw>
         <Square value={squares[6]} onClick={() => onClick(6)}/>
         <Square value={squares[7]} onClick={() => onClick(7)}/>
         <Square value={squares[8]} onClick={() => onClick(8)}/>
-      </div>
+      </ BoardRaw>
     </div>
   );
 }
