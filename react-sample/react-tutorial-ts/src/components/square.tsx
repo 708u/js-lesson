1e1aaa356fa8a2ce49bfd5a9e8a9d8ede9c24ff1
708u <naoya.uda@showcase-gig.com>
2020-11-23 13:10:47 +0900

    feat: add square styled component

--- react-sample/react-tutorial-ts/src/components/square.tsx
+++ react-sample/react-tutorial-ts/src/components/square.tsx
@@ -1,4 +1,5 @@
 import React, { FC, MouseEvent } from 'react';
+import styled from 'styled-components';
 
 export type square = 'O' | 'X' | null;
 
@@ -7,12 +8,27 @@ type Props = {
   value: square
 }
 
+const SquareBtn = styled.button`
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 34px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 34px;
+`;
+
 const Square: FC<Props> = (props) => {
   const { onClick, value } = props;
   return (
-    <button className="square" onClick={ onClick }>
+    <SquareBtn onClick={ onClick }>
       { value }
-    </button>
+    </ SquareBtn>
   );
 }
 
