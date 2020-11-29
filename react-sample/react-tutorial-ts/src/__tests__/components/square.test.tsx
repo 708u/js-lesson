7101a75fadacff920d60ef2ed83ed8ea711c6c4f
708u <naoya.uda@showcase-gig.com>
2020-11-29 20:39:26 +0900

    feat: move tests

--- react-sample/react-tutorial-ts/src/__tests__/components/square.test.tsx
+++ react-sample/react-tutorial-ts/src/__tests__/components/square.test.tsx
@@ -5,13 +5,13 @@ import Square, { SquareBtn } from 'components/square';
 
 describe('square test', () => {
   it('should render as expected components', () => {
-    const component = renderer.create(<Square value="X" onClick={() => {}} />);
+    const component = renderer.create(<Square value="X" onClick={() => true} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('has button', () => {
-    const buttonWrapper = shallow(<Square value="X" onClick={() => {}} />).find(
+    const buttonWrapper = shallow(<Square value="X" onClick={() => true} />).find(
       SquareBtn
     );
     expect(buttonWrapper).toHaveLength(1);
